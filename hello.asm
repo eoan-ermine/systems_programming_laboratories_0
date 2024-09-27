80400f730d68702f8d81657ffc2bd40d0ed470f1
Danil Sidoruk <danilsidoruknv@yandex.ru>
2024-09-27 09:53:08 +0300

    Add comments to source code

--- hello.asm
+++ hello.asm
@@ -1,14 +1,14 @@
-section .data
-    hello: db 'Hello world!',10
-    helloLen: equ $-hello
+section .data ; данные должны лежать в секции для данных
+    hello: db 'Hello world!',10 ; db используется для объявления инициализированных данных
+    helloLen: equ $-hello ; equ используется для объявления констант
 
 global main
-section .text
+section .text ; код должен лежать в секции .text
 
 main:
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, hello
-    mov edx, helloLen
-    int 80h
-    ret
+    mov eax, 4 ; eax - syscall: 4 - write
+    mov ebx, 1 ; ebx - файловый дескриптор: 1 - stdout
+    mov ecx, hello ; ecx - буффер
+    mov edx, helloLen ; edx - длина данных в переданном буффере
+    int 80h ; вызываем прерывание - обработчик системных вызовов
+    ret ; выход из программы
